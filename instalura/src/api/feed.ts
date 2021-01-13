9a5dc43c59b91c56c22e61ad78baa2ea1c3140a0
Alexandre Tannus <alexandretannus@gmail.com>
2021-01-13 18:19:08 -0300

    Alterações para portabilidade para iOS

--- instalura/src/api/feed.ts
+++ instalura/src/api/feed.ts
@@ -1,9 +1,9 @@
 import { Dispatch } from "react";
-
-const fetchUrl = "http://192.168.0.2:3030/feed"
+import { Platform } from "react-native";
 
 const lerFotos = async(callback: Dispatch<React.SetStateAction<never[]>>) => {
-    const fotosHttp = await fetch(fetchUrl);
+    const fetchUrl = Platform.OS === "ios" ? "localhost" : "192.168.0.2"
+    const fotosHttp = await fetch(`http://${fetchUrl}:3030/feed`);
     const fotosJson = await fotosHttp.json();
     callback(fotosJson);
 }
